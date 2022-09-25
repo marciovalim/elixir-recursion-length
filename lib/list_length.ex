4f19e274d1a0cac9a1d79d1e2a97355f636c3c41
marciovalim <marciogsvalim@gmail.com>
2022-09-25 07:36:27 -0300

    implemented

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    length(list, 0)
+  end
+
+  defp length([], acc) do
+    acc
+  end
+
+  defp length([_ | tail], acc) do
+    length(tail, acc + 1)
   end
 end
